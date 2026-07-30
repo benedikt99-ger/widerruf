540fa73700e18a0aa46a1977e5cfc75773469de0
benedikt99-ger <benedikt99@gmail.com>
2026-07-30 09:11:46 +0200

    Add files via upload

---
+++ metadata.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Copyright © benedikt nünemann. All rights reserved.
+ */
+
+$sMetadataVersion = '2.1';
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => \nuenemann\widerruf\Module::MODULE_ID,
+    'title'       => 'Widerruf Formular für OXID 7',
+    'description' => 'Widerruf Formular für OXID 7',
+    'thumbnail'   => 'bn_logo.png',
+    'version'     => '0.1.0',
+    'author'      => 'Nünemann',
+    'url'         => 'https://github.com/benedikt99-ger/widerruf',
+    'email'       => 'benedikt@nuenemann.de',
+	'extend' => [
+		\OxidEsales\Eshop\Application\Model\Order::class => \nuenemann\widerruf\Model\Order::class,
+	],
+    'controllers' => [
+        'OrderController'  => \nuenemann\widerruf\Controller\Admin\WiderrufController::class,
+        'ordertracking'    => \nuenemann\widerruf\Controller\Admin\OrderTrackingController::class,
+    ],	
+    'templates' => [
+        'withdrawalform.tpl' => 'nuenemann/widerruf/Application/views/widerrufform.tpl',
+        'withdrawalEmailHtml.tpl' => 'nuenemann/widerruf/Application/views/widerrufEmailHtml.tpl',
+        'withdrawalEmailPlain.tpl' => 'nuenemann/widerruf/Application/views/widerrufEmailPlain.tpl'
+    ],	
+   'settings' => [
+        [
+            'group' => 'WiderrufMain','name' => 'WiderrufSitekey',
+            'type' => 'str','value' => '','position' => 0
+        ],
+        [
+            'group' => 'WiderrufMain','name' => 'WiderrufSecret',
+            'type' => 'str','value' => '','position' => 1
+        ],
+        [
+            'group' => 'WiderrufMain','name' => 'WiderrufReasons',
+            'type' => 'aarr','value' => [
+                'small' => 'zu klein',
+                'large' => 'zu groß',
+                'dislike' => 'nicht gefallen'
+            ],'position' => 2
+        ],
+        [
+            'group' => 'WiderrufMain','name' => 'WiderrufEmail',
+            'type' => 'str','value' => '','position' => 3
+        ],
+        [
+            'group' => 'WiderrufMain','name' => 'WiderrufCC',
+            'type' => 'arr','value' => [],'position' => 4
+        ],
+        [
+            'group' => 'WiderrufMain','name' => 'WiderrufRetoureportal',
+            'type' => 'str','value' => '','position' => 5
+        ],
+        [
+            'group' => 'WiderrufMain','name' => 'WiderrufBootstrapVersion',
+            'type' => 'select','value' => 'v3','constraints' => 'v3|v4','position' => 6
+        ]
+    ]
+];
