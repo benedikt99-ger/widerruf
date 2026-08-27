5d39544d479364f9ce894a4cc4b62457c66a253c
benedikt99-ger <benedikt99@gmail.com>
2026-08-27 10:58:13 +0200

    Add files via upload

--- views/admin_twig/de/module_options.php
+++ views/admin_twig/de/module_options.php
@@ -3,14 +3,9 @@ $sLangName = "Deutsch";
 $style = '<style type="text/css">.groupExp a.rc b {font-size: medium; color: #ff3600; }.groupExp dt input.txt { width: 422px !important} .groupExp dl { display: block !important; } input.confinput {position: fixed; top: 20px; right: 70px; background: #008B2D; padding: 10px 25px; color: white; border: 1px solid black; cursor:pointer; font-size: 125%; } input.confinput:hover {outline: 3px solid #ff3600;} .groupExp dt textarea.txtfield {width: 430px; height: 100px;}</style>';
 $aLang = [
     'charset' => 'UTF-8',
-    'SHOP_MODULE_GROUP_WiderrufMain' => $style . 'Einstellungen',
-    'SHOP_MODULE_WiderrufSitekey' => "reCaptcha v2 Site Key <a href='https://www.google.com/recaptcha/admin' target='_blank'>( ? )</a>",
-    'SHOP_MODULE_WiderrufSecret' => "reCaptcha v2 Secret <a href='https://www.google.com/recaptcha/admin' target='_blank'>( ? )</a>",
-    'SHOP_MODULE_WiderrufReasons' => "eigene Gründe, Format:<br/>abkuerzung => ausgeschriebener Name",
-    'SHOP_MODULE_WiderrufEmail' => "abweichendes Empfänger (sonst Adresse für Bestellungen)",
-    'SHOP_MODULE_WiderrufCC' => "weitere CC Empfänger",
-    'SHOP_MODULE_WiderrufRetoureportal' => "Links zum externen Retouren-Portal (z.B. von DHL)",
-    'SHOP_MODULE_WiderrufBootstrapVersion' => 'Bootstrap Version des Templates (v3 für Flow oder v4 für Wave)',
-    'SHOP_MODULE_WiderrufBootstrapVersion_v3' => 'v3',
-    'SHOP_MODULE_WiderrufBootstrapVersion_v4' => 'v4',
+    'SHOP_MODULE_GROUP_CookieConsentMain' => $style . 'Einstellungen',
+    'SHOP_MODULE_CookieconsentInfoLink' => "Informations Link ",
+    'SHOP_MODULE_CookieconsentCategories' => "Kategorien, Format:<br/>abkuerzung => ausgeschriebener Name",
+    'SHOP_MODULE_CookieConsentEmail' => "abweichendes Empfänger (sonst Adresse für Bestellungen)",
+    'SHOP_MODULE_CookieConsentCC' => "weitere CC Empfänger",
 ];
